93c1178f32c62c4227f237d346bb408abe626811
Sam Schmidt <samuel@dersam.net>
2014-11-01 15:49:31 -0400

    basic crontab generation from a sqlite DB

---
+++ pica.py
@@ -0,0 +1,22 @@
+import sqlite3
+import sys
+import subprocess
+
+#get jobs
+conn = sqlite3.connect("example/sample.db")
+c = conn.cursor()
+c.execute("SELECT command, minute, hour, day_month, month, day_week, label "
+          "FROM crontab WHERE enabled = 1 ORDER BY hour DESC minute DESC")
+jobs = c.fetchall()
+conn.close()
+
+#write crontab file
+crontab = open("crontab.cr", 'w')
+for job in jobs:
+    line = '#'+job[6]+"\n"
+    line = line + job[1] + job[2] + job[3] + job[4] + job[5]+"\n"
+    crontab.write(line)
+
+crontab.close()
+
+subprocess.call('crontab crontab.cr')
