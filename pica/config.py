e7a6596d5a3fa3e85344fa064524ee1faf90fca9
Sam Schmidt <samuel@dersam.net>
2014-11-01 21:47:29 -0400

    config var getter

--- pica/config.py
+++ pica/config.py
@@ -3,8 +3,19 @@ __author__ = 'Sam'
 
 import ConfigParser
 
-params = []
+params = None
+
 
 def load(path):
+    global params
     params = ConfigParser.ConfigParser()
     params.read(path)
+
+
+def get(section, var):
+    p = params.get(section, var)
+    if p.lower() == 'yes':
+        return True
+    elif p.lower() == 'no':
+        return False
+    return p
